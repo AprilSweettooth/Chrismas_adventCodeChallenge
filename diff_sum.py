53260df5c0a23ddc051690f48e2647aeb611f77c
Peter <sqhy2@cam.ac.uk>
2021-12-01 22:55:55 +0000

    day1

---
+++ diff_sum.py
@@ -0,0 +1,22 @@
+def sum(x):
+    count = 0
+    for i in range(len(x)-1):
+        if x[i] < x[i+1]:
+            count += 1
+    return count
+
+
+txtfile = open('data.txt')
+L = []
+for line in txtfile:
+    L.append(int(line.rstrip()))
+txtfile.close()
+print(sum(L))
+
+M = []
+for i in range(len(L)-2):
+    count = 0
+    num = L[i] + L[i+1] + L[i+2]
+    M.append(num)
+
+print(sum(M))
